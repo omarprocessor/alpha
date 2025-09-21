fc6fd53d3587a6884a10043d34a279c3b0bc6f63
Samson Moturi <samsonmoturi79@gmail.com>
2025-09-22 00:43:10 +0300

    Automatically sweeps a servo motor back and forth continuously. */
    Automatically sweeps a servo motor back and forth continuously.
    */

---
+++ examples/servo-sweep-pot/servo-sweep-pot.ino
@@ -0,0 +1,34 @@
+/**
+
+Title: Automated Servo Sweep
+
+Description: Automatically sweeps a servo motor back and forth continuously.
+*/
+
+#include <Servo.h>
+
+#include "alpha.h"
+
+Servo myServo;
+int pos = 0;
+int sweepDirection = 1;
+
+void setup() {
+myServo.attach(AL_SERVO_D5);
+}
+
+void loop() {
+// Move the servo
+myServo.write(pos);
+
+// Update position for next iteration
+pos += sweepDirection;
+
+// Reverse direction at limits
+if (pos >= 180 || pos <= 0) {
+sweepDirection *= -1;
+}
+
+// Control sweep speed
+delay(15);
+}
